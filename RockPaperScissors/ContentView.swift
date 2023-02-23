703b92593f9f9612b315bb30723f5b5d9c1dbf58
gitkoDev <prostodenis1397@gmail.com>
2023-02-23 09:05:57 +0800

    - add lizard and spock - add wrapping stack for them

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import WrappingStack
 
 struct ContentView: View {
 	@State private var userScore = 0
@@ -21,7 +22,7 @@ struct ContentView: View {
 	let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 	@State private var timerIsRunning = false
 	
-	let imagesNames = ["rock", "paper", "scissors"]
+	let imagesNames = ["rock", "paper", "scissors", "lizard", "spock"]
 	
 	let timerDelay: Double = 1
 
@@ -40,49 +41,36 @@ struct ContentView: View {
 						Button {
 							startCountdown()
 						} label: {
-							Text(String(timeRemaining))
+							Text(timeRemaining > 0 ? String(timeRemaining) : "GO!")
 								.onChange(of: timeRemaining) { _ in
 //										Apply changes to the text view during every second of the countdown
 
 								}
-								.countdownModifier()
 								.onReceive(timer) { _ in
 
-									if timerIsRunning && timeRemaining >= 1 {
+									if timerIsRunning && timeRemaining >= 0 {
 										timeRemaining -= 1
 									} else {
 										timerIsRunning = false
 									}
 								}
+								.countdownModifier()
 						}
 						
 						
-						if timerIsRunning {
-							Text("timer is running")
-								.opacity(0)
+						if timeRemaining > 0 && userChoseSomething {
+							Text("too early")
+								.opacity(1)
 								.animation(.easeInOut(duration: 1))
 								.outcomeTitleStyle()
+						} else if timeRemaining == 0 && userChoseSomething {
+							Text("Choice made")
+								.outcomeTitleStyle()
 						} else {
-							Text("timer stopped")
+							Text("stopped")
 								.outcomeTitleStyle()
 						}
-							
-
-//							if userChoseSomething && isFinishedInADraw {
-//								Text("It's a draw")
-//									.outcomeTitleStyle()
-//							} else if userChoseSomething && userWon {
-//								Text("You won")
-//									.outcomeTitleStyle()
-//							} else if userChoseSomething && !userWon {
-//								Text("You lost")
-//									.outcomeTitleStyle()
-//							} else {
-//								Text("Make your choice")
-//									.opacity(0)
-//									.outcomeTitleStyle()
-//							}
-
+						
 						Image(computersChoice)
 							.resizable()
 							.frame(width: 80, height: 80)
@@ -96,9 +84,8 @@ struct ContentView: View {
 						
 	//					Create a frame to store the picture of the computer's choice
 						
-						HStack (spacing: 30){
+						WrappingHStack(id: \.self, alignment: .center, horizontalSpacing: 30, verticalSpacing: 15) {
 							ForEach(imagesNames, id: \.self) { obj in
-				
 								Button {
 									chooseAnObject(userObj: obj, computerObj: imagesNames.randomElement()!)
 								} label: {
@@ -107,12 +94,13 @@ struct ContentView: View {
 								}
 								.frame(width: 50, height: 50)
 								.padding()
-								.background(.ultraThinMaterial)
+								.background(.linearGradient(colors: [.blue, .mint], startPoint: .bottom, endPoint: .top))
 								.clipShape(Circle())
 							}
+						
+						
 						}
 						
-						Spacer()
 						
 						Text("score: \(userScore)")
 							.font(.title2.weight(.medium))
@@ -199,7 +187,7 @@ struct ContentView: View {
 	func startCountdown() -> Void {
 		timeRemaining = 3
 		
-		DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
 			timerIsRunning = true
 		}
 
@@ -225,7 +213,7 @@ struct Countdown: ViewModifier {
 			.frame(width: 110, height: 110)
 			.background(.ultraThinMaterial)
 			.clipShape(Circle())
-			.font(.system(size: 100, weight: .bold))
+			.font(.system(size: 50, weight: .bold))
 			.opacity(0.7)
 			.foregroundColor(.purple)
 	}
